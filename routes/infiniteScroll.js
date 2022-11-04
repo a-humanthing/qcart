061d3752c2be9e296334c79998fc8893222d5d4c
a-humanthing <arunkrishnakt2002@gmail.com>
2022-11-04 06:44:12 +0530

    update after completion of inifinite scroll home

--- routes/infiniteScroll.js
+++ routes/infiniteScroll.js
@@ -6,10 +6,16 @@ router.post('/',async(req,res,next)=>{
     const {count} = req.body;
     console.log('count=',count);
     const product = await Product.find().sort('price');
-    const filterP = await Product.find().sort('price').skip(count).limit(1);
+    const filterP = await Product.find().sort('price').skip(count*3).limit(3);
     console.log('pro',product);
     console.log('fi',filterP);
     res.json({product,filterP})
+})
+
+router.post('/baseload',async(req,res,next)=>{
+    const {baseCount}=req.body;
+    const baseLoad = await Product.find().sort('price').limit(3);
+    res.json({baseLoad});
 })
 
 module.exports=router;
