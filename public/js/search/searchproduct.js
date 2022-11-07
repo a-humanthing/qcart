41a51f19fbffa169cbaf1c478dcde26e16021e32
a-humanthing <arunkrishnakt2002@gmail.com>
2022-11-07 12:52:59 +0530

    update after mobile search

--- public/js/search/searchproduct.js
+++ public/js/search/searchproduct.js
@@ -24,3 +24,30 @@ function searchProduct(){
 
     })
 }
+
+function mSearchProduct(){
+    const keyword = document.getElementById('inputMobileSearch').value;
+    fetch('/loadproduct/search',{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({keyword})
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        if(data.notFound){
+            alert('item not found')
+        }else{
+            const matchList = document.getElementById('msearchmatch');
+            matchList.innerHTML='';
+            if(data.matchedProduct.length>0){
+                for(let pro of data.matchedProduct){
+                    matchList.innerHTML+=`<h3><a href="/product/${pro._id}" style="text-decoration:none" class="text-success">${pro.productname}</a></h3>`
+                }
+            }else{
+                matchList.innerHTML+='<h3 class="text-danger">Match Not Found</h3>'
+            }
+        }
+       
+
+    })
+}
