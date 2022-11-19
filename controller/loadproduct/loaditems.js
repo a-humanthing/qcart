42507f7e04a3fe4cb10ae69b24669ace6e62f018
a-humanthing <arunkrishnakt2002@gmail.com>
2022-11-19 11:02:41 +0530

    somthing in load i don't know exactly

--- controller/loadproduct/loaditems.js
+++ controller/loadproduct/loaditems.js
@@ -2,7 +2,31 @@ const Product = require("../../model/products");
 
 module.exports.baseLoad = async (req, res, next) => {
   const { baseCount } = req.body;
+  const withrating = await Product.aggregate([
+    { $match: {} },
+    { $unwind: "$reviews" },
+    {
+      $lookup: {
+        from: "reviews",
+        localField: "reviews",
+        foreignField: "_id",
+        as: "rating",
+      },
+    },
+    { $unwind: "$rating" },
+    { $group: { _id: "$_id", star: { $sum: "$rating.rating" } } }
+  ]);
+  console.log("reviews", withrating);
   const baseLoad = await Product.find().sort("price").limit(3);
+  console.log('base',baseLoad);
+  baseLoad.forEach(item=>{
+    for(let rat of withrating){
+      if(item._id===rat._id){
+        Object.assign(item,{star:`${rat.rating}`});
+      }
+    }
+  })
+  console.log('dummyload',baseLoad);
   res.json({ baseLoad });
 };
 
